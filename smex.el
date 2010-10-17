c3ddd0ceeacf743a2b5e07d2aaf9dc19c65ea223
Michael Budde <mbudde@gmail.com>
2010-10-17 15:55:50 +0200

    Add alternative keybinding for smex-describe-function

--- smex.el
+++ smex.el
@@ -116,6 +116,7 @@ Must be set before initializing Smex."
 
 (defun smex-prepare-ido-bindings ()
   (define-key ido-completion-map (kbd "C-h f") 'smex-describe-function)
+  (define-key ido-completion-map (kbd "<f1> f") 'smex-describe-function)
   (define-key ido-completion-map (kbd "M-.") 'smex-find-function)
   (define-key ido-completion-map (kbd "C-a") 'move-beginning-of-line))
 
